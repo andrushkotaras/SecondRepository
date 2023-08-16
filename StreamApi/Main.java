012980bfcc79008c7a881016e3351e8cff001edc
andrushkotaras <tarasandrushko2000@gmail.com>
2023-08-16 13:31:59 +0300

    Create README.md

--- StreamApi/Main.java
+++ StreamApi/Main.java
@@ -10,6 +10,21 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
 
+        List<String> surNames = Stream.of("Andrushko", "Andrushko","Bulavin","Gents",
+                "Gents").collect(Collectors.toMap());
+
+
+
+
+
+
+
+
+
+
+
+
+
         List<Integer> numbers = Arrays.asList(2,3,4,4,45,6,7,8);
         int sum = numbers.stream().mapToInt(Integer::intValue).sum();
         System.out.println(sum);
