872ce2bdac80cadcdb8384597d3e7e8fb8c6870e
andrushkotaras <tarasandrushko2000@gmail.com>
2023-08-17 02:27:04 +0300

    Create README.md

--- StreamApi/Main.java
+++ StreamApi/Main.java
@@ -6,26 +6,21 @@ import java.util.function.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Objects;
+import java.util.Arrays;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<String> surNames = Stream.of("Andrushko", "Andrushko","Bulavin","Gents",
-                "Gents").collect(Collectors.toMap());
+        List<String> surNames = Stream.of("Andrushko", "Bulavin", "Gents","Andrushko", "Bulavin", "Gents",
+                "Andrushko", "Bulavin", "Gents","Andrushko", "Bulavin", "Gents").toList();
+        List<String> stream = surNames.stream()
+                .collect(Collectors.toMap(str -> str, str -> str.??????));
 
 
 
-
-
-
-
-
-
-
-
-
-
-        List<Integer> numbers = Arrays.asList(2,3,4,4,45,6,7,8);
+        List<Integer> numbers = Arrays.asList(2, 3, 4, 4, 45, 6, 7, 8);
         int sum = numbers.stream().mapToInt(Integer::intValue).sum();
         System.out.println(sum);
 
